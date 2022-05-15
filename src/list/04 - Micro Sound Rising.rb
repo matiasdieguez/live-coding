106d2ffd66e6150ed0a5825737c9763d49bf03c4
Matías Dieguez <matiasdieguez@hotmail.com>
2022-05-14 21:56:59 -0300

    Add amp

--- src/list/04 - Micro Sound Rising.rb
+++ src/list/04 - Micro Sound Rising.rb
@@ -8,36 +8,37 @@
 # pad sleep 3.4 after & 48 before
 
 loop_sleep = 6.86
+loop_amp = 3
 
 live_loop :beats do
   2.times do
-    sample :msr_beats_1
+    sample :msr_beats_1, amp: loop_amp
     sleep loop_sleep
   end
   2.times do
-    sample :msr_beats_2
+    sample :msr_beats_2, amp: loop_amp
     sleep loop_sleep
   end
   2.times do
-    sample :msr_beats_1
-    sample :msr_beats_2
+    sample :msr_beats_1, amp: loop_amp
+    sample :msr_beats_2, amp: loop_amp
     sleep 6.86
   end
 end
 
 live_loop :pad do
   sleep 3.4
-  sample :msr_pad
+  sample :msr_pad, amp: loop_amp
   sleep 48
 end
 
 live_loop :bass do
   1.times do
-    sample :msr_bass_1
+    sample :msr_bass_1, amp: loop_amp
     sleep loop_sleep
   end
   8.times do
-    sample :msr_bass_2
+    sample :msr_bass_2, amp: loop_amp
     sleep loop_sleep
   end
 end
